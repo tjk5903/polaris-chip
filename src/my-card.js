4c0bc1e853c13358c5f2ae17810f8b59fb7f8a3e
tjk5903 <tjk5903@psu.edu>
2024-02-04 13:57:36 -0500

    polaris-chipDone

--- src/my-card.js
+++ src/my-card.js
@@ -21,6 +21,67 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+
+      a {
+  text-decoration: none;
+}
+#cardlist {
+  display: flex;
+}
+
+.btn-wrapper {
+  background-color: white;
+  padding: 10px;
+  margin: 5px;
+}
+
+
+.btn {
+  background-color: navy;
+  color: white;
+  font-size: 16px;
+  border-radius: 10%;
+  padding: 4px 4px 4px 4px;
+  margin: 4px 4px 4px 4px
+}
+
+.btn:focus,
+.btn:hover {
+  background-color: black;
+}
+.card {
+  width: 100%;
+  max-width: 400px;
+  border: 5px solid navy;
+  padding: 8px;
+}
+.card-content{
+  border: 2px solid navy;
+  padding: 2px;
+}
+.change-color {
+  background-color: orange;
+}
+
+
+@media only screen and (max-width: 800px) and (min-width: 501px) {
+  .details-button {
+    display: block;
+  }
+}
+.details-button {
+  display: none;
+}
+
+@media only screen and (max-width: 500px) {
+  .card {
+    max-width: 100%;
+  }
+
+  .card img {
+    max-height: none;
+  }
+}
     `;
   }
 
