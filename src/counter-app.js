a86fb9b3784d7fadda8a2e813d0ac597bc807a90
tjk5903 <tjk5903@psu.edu>
2024-02-15 12:17:32 -0500

    counterApp

--- src/counter-app.js
+++ src/counter-app.js
@@ -13,6 +13,75 @@ export class CounterApp extends LitElement {
   constructor() {
     super();
     this.title = "Counter App";
+    this.counter = "16";
   }
-  
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        border: 1px solid black;
+        margin: 20px;
+        padding: 20px;
 }
+a {
+  text-decoration: none;
+}
+#counterApp {
+  display: linear;
+}
+
+.btn-wrapper {
+  background-color: white;
+  padding: 10px;
+  margin: 5px;
+}
+
+.btn {
+  background-color: navy;
+  color: white;
+  font-size: 16px;
+  border-radius: 10%;
+  padding: 4px 4px 4px 4px;
+  margin: 4px 4px 4px 4px
+}
+
+.btn:focus,
+.btn:hover {
+  background-color: black;
+}
+.card {
+  width: 100%;
+  max-width: 830px;
+  border: 5px solid navy;
+  padding: 8px;
+}
+.card-content{
+  border: 2px solid navy;
+  padding: 2px;
+}
+.change-color {
+  background-color: orange;
+}
+    `;
+  }  
+  render() {
+    return html`
+    <div class="counterApp"></div>
+    <counter-app counter="16" min="10" max="25"></counter-app>
+    </div>`;
+  }
+
+  static get properties() {
+    return {
+      label: { type: String, reflect: true },
+      image: { type: String, reflect: true },
+      bodyText: { type: String },
+      description: { type: String, reflect: true},
+      link: { type: String },
+      fancy: { type: Boolean, reflect: true },
+    };
+  }
+}
+
+globalThis.customElements.define(MyCard.tag, MyCard);
+
