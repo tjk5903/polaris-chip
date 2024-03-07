11cb77499863eeba1d317a1db3bb3f7c87572f8e
tjk5903 <tjk5903@psu.edu>
2024-03-07 15:17:43 -0500

    render changes

--- src/campus-alert.js
+++ src/campus-alert.js
@@ -28,12 +28,16 @@ export class CampusAlert extends LitElement {
         display: flex;
         flex-direction: column;
         border: 2px solid seashell;
-        background-color: #e6e20b;
+        background-color: #e4cf17;
         margin: 50px;
         padding: 25px;
         max-width: 1000px;
         height: 50px;
         align-items: center;
+      }
+
+      .alert {
+        font-weight: bold;
       }
   
       .date {
@@ -114,26 +118,26 @@ export class CampusAlert extends LitElement {
 
   toggleAlert() {
     this.opened = !this.opened;
-    
-    const container = this.shadowRoot.querySelector('.closedContainer');
+
+    const container = this.shadowRoot.querySelector('.campus-alert');
     container.classList.toggle('opened', this.opened);
     container.classList.toggle('closed', !this.opened);
-    
+
     if (!this.opened) {
       localStorage.setItem('campus-alert-opened-state', 'false');
     } else {
       localStorage.removeItem('campus-alert-opened-state');
     }
-  }
+}
 
-  render() {
+render() {
     return html`
       <div class="closedContainer ${(this.sticky) ? "sticky" : ""}">
         <div class ="closed-toggle-button" @click="${this.toggleAlert}">
           ${this.opened ? html`
             <svg xmlns="http://www.w3.org/2000/svg" style="height: 50px; width: 50px;" viewBox="0 0 24 24"><title>chevron-down</title><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
           ` : html`
-            <svg xmlns="http://www.w3.org/2000/svg" style="height: 50px; width: 50px;" viewBox="0 0 24 24"><title>alert-circle-outline</title><path d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" style="height: 50px; width: 50px; visibility: hidden;" viewBox="0 0 24 24"><title>alert-circle-outline</title><path d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z" /></svg>
           `}
           Alert!
         </div>
@@ -147,7 +151,7 @@ export class CampusAlert extends LitElement {
         </div>
       </div>
     `;
-  }
+}
 
 static get properties() {
   return {
