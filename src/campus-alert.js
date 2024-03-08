dff8f2f0f49c2dd5e0abaf2e8aa399b850a5bbe3
tjk5903 <tjk5903@psu.edu>
2024-03-08 14:23:31 -0500

    3-8

--- src/campus-alert.js
+++ src/campus-alert.js
@@ -143,6 +143,7 @@ export class CampusAlert extends LitElement {
   
       .closed {
         height: var(--closed-height, 50px);
+        width: 100%;
         background-color: var(--alert-background-color, blue);
       }
   
@@ -187,15 +188,26 @@ export class CampusAlert extends LitElement {
     this.style.setProperty('--alert-text-color', textColor);
     this.style.setProperty('--alert-background-color', backgroundColor);
 
+    // Define CSS classes for info button based on alert state
+    const infoButtonClass = this.opened ? 'info-button-opened' : 'info-button-closed';
+
     return html`
     <div class="closedContainer" style="${this.sticky && this.stickyIndex ? 'position: sticky; top: 0;' : ''}">
       <div class="campus-alert ${this.opened ? 'opened' : 'closed'}">
         ${this.opened ? html`
           <div class="date">${formattedDate}</div>
           <button class="close-button" @click="${this.closeBanner}">&times; Close</button>
+          <div class="${infoButtonClass}">
+            <!-- Info button content -->
+            <svg fill="#000000" height="38px" width="38px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 27.963 27.963" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g id="c129_exclamation"> <path d="M13.983,0C6.261,0,0.001,6.259,0.001,13.979c0,7.724,6.26,13.984,13.982,13.984s13.98-6.261,13.98-13.984 C27.963,6.259,21.705,0,13.983,0z M13.983,26.531c-6.933,0-12.55-5.62-12.55-12.553c0-6.93,5.617-12.548,12.55-12.548 c6.931,0,12.549,5.618,12.549,12.548C26.531,20.911,20.913,26.531,13.983,26.531z"></path> <polygon points="15.579,17.158 16.191,4.579 11.804,4.579 12.414,17.158 "></polygon> <path d="M13.998,18.546c-1.471,0-2.5,1.029-2.5,2.526c0,1.443,0.999,2.528,2.444,2.528h0.056c1.499,0,2.469-1.085,2.469-2.528 C16.441,19.575,15.468,18.546,13.998,18.546z"></path> </g> <g id="Capa_1_207_"> </g> </g> </g></svg>
+          </div>
         ` : html`
           <div class="closed-text">
-            Text Campus Alert
+            <div class="${infoButtonClass}">
+              <!-- Info button content -->
+              <svg fill="#000000" height="38px" width="38px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 27.963 27.963" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g id="c129_exclamation"> <path d="M13.983,0C6.261,0,0.001,6.259,0.001,13.979c0,7.724,6.26,13.984,13.982,13.984s13.98-6.261,13.98-13.984 C27.963,6.259,21.705,0,13.983,0z M13.983,26.531c-6.933,0-12.55-5.62-12.55-12.553c0-6.93,5.617-12.548,12.55-12.548 c6.931,0,12.549,5.618,12.549,12.548C26.531,20.911,20.913,26.531,13.983,26.531z"></path> <polygon points="15.579,17.158 16.191,4.579 11.804,4.579 12.414,17.158 "></polygon> <path d="M13.998,18.546c-1.471,0-2.5,1.029-2.5,2.526c0,1.443,0.999,2.528,2.444,2.528h0.056c1.499,0,2.469-1.085,2.469-2.528 C16.441,19.575,15.468,18.546,13.998,18.546z"></path> </g> <g id="Capa_1_207_"> </g> </g> </g></svg>
+            </div>
+            TEST CAMPUS ALERT
             <div class="closed-toggle-button" @click="${this.toggleAlert}">
               <svg xmlns="http://www.w3.org/2000/svg" style="height: 50px; width: 50px;" viewBox="0 0 24 24">
                 <title>chevron-down</title>
@@ -204,9 +216,6 @@ export class CampusAlert extends LitElement {
             </div>
           </div>
         `}
-        <div class="info-button">
-        <svg fill="#000000" height="38px" width="38px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 27.963 27.963" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g id="c129_exclamation"> <path d="M13.983,0C6.261,0,0.001,6.259,0.001,13.979c0,7.724,6.26,13.984,13.982,13.984s13.98-6.261,13.98-13.984 C27.963,6.259,21.705,0,13.983,0z M13.983,26.531c-6.933,0-12.55-5.62-12.55-12.553c0-6.93,5.617-12.548,12.55-12.548 c6.931,0,12.549,5.618,12.549,12.548C26.531,20.911,20.913,26.531,13.983,26.531z"></path> <polygon points="15.579,17.158 16.191,4.579 11.804,4.579 12.414,17.158 "></polygon> <path d="M13.998,18.546c-1.471,0-2.5,1.029-2.5,2.526c0,1.443,0.999,2.528,2.444,2.528h0.056c1.499,0,2.469-1.085,2.469-2.528 C16.441,19.575,15.468,18.546,13.998,18.546z"></path> </g> <g id="Capa_1_207_"> </g> </g> </g></svg>
-        </div>
         <div class="message-container">
           <div class="slanted-card" style="display: ${this.opened ? 'flex' : 'none'}">
             ${this.message}
