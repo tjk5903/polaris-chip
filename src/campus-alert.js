f6eac9659281ed5afbeb575bbdd21ba4310c907a
tjk5903 <tjk5903@psu.edu>
2024-03-09 12:14:06 -0500

    3-9

--- src/campus-alert.js
+++ src/campus-alert.js
@@ -13,7 +13,7 @@ export class CampusAlert extends LitElement {
     this.level = 'notice'; // Default level
     this.date = '';
     this.sticky = false;
-    this.opened = localStorage.getItem('campus-alert-opened-state') === "true";
+    this.opened = true;
     this.closedHeight = '50px'; 
     this.openHeight = '200px'; 
     this.stickyIndex = localStorage.getItem('campus-alert-sticky-index') === "true";
@@ -68,10 +68,10 @@ export class CampusAlert extends LitElement {
         color: var(--alert-date-color, white);
       }
       .sticky {
-  position: fixed;
-  top: 0;
-  width: 100%;
-}
+        position: fixed;
+        top: 0;
+        width: 100%;
+      }
 
       .closedContainer {
         display: flex;
@@ -82,14 +82,14 @@ export class CampusAlert extends LitElement {
       .closed-text {
         color: black;
         font-weight: bold;
-  display: flex;
-  justify-content: center; /* Horizontally center the text */
-  align-items: center; /* Vertically center the text */
-  height: 100%; /* Ensure the div takes up the full height of its parent */
-}
-.closed-toggle-button {
-  margin-left: 10px; /* Add some spacing between text and arrow */
-}
+        display: flex;
+        justify-content: center; /* Horizontally center the text */
+        align-items: center; /* Vertically center the text */
+        height: 100%; /* Ensure the div takes up the full height of its parent */
+      }
+      .closed-toggle-button {
+        margin-left: 10px; /* Add some spacing between text and arrow */
+      }
 
       .campus-alert {
         position: relative;
@@ -157,8 +157,12 @@ export class CampusAlert extends LitElement {
       }
       .info-button-closed {
         position: absolute;
-        left: 330px; 
-}
+        left: 335px;
+      }
+      .info-button-open {
+        position: absolute;
+        left: 200px;
+      } 
     `;
   }
 
@@ -166,7 +170,7 @@ export class CampusAlert extends LitElement {
     const currentDate = new Date();
     const options = { month: 'long', day: '2-digit', year: 'numeric', hour: 'numeric', minute: '2-digit', hour12: true };
     const formattedDate = currentDate.toLocaleString('en-US', options).toUpperCase();
-
+    
     // Define color variables based on importance attribute
     let textColor = 'black';
     let backgroundColor = 'blue';
@@ -196,16 +200,21 @@ export class CampusAlert extends LitElement {
     const infoButtonClass = this.opened ? 'info-button-opened' : 'info-button-closed';
 
     return html`
-    <div class="closedContainer" style="${this.sticky && this.stickyIndex ? 'position: sticky; top: 0;' : ''}">
-      <div class="campus-alert ${this.opened ? 'opened' : 'closed'}">
-        ${this.opened ? html`
-          <div class="date">${formattedDate}</div>
-          <button class="close-button" @click="${this.closeBanner}">&times; Close</button>
-          <div class="${infoButtonClass}">
-            <!-- Info button content -->
-            <svg fill="#000000" height="30px" width="30px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 27.963 27.963" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g id="c129_exclamation"> <path d="M13.983,0C6.261,0,0.001,6.259,0.001,13.979c0,7.724,6.26,13.984,13.982,13.984s13.98-6.261,13.98-13.984 C27.963,6.259,21.705,0,13.983,0z M13.983,26.531c-6.933,0-12.55-5.62-12.55-12.553c0-6.93,5.617-12.548,12.55-12.548 c6.931,0,12.549,5.618,12.549,12.548C26.531,20.911,20.913,26.531,13.983,26.531z"></path> <polygon points="15.579,17.158 16.191,4.579 11.804,4.579 12.414,17.158 "></polygon> <path d="M13.998,18.546c-1.471,0-2.5,1.029-2.5,2.526c0,1.443,0.999,2.528,2.444,2.528h0.056c1.499,0,2.469-1.085,2.469-2.528 C16.441,19.575,15.468,18.546,13.998,18.546z"></path> </g> <g id="Capa_1_207_"> </g> </g> </g></svg>
+  <div class="closedContainer" style="${this.sticky && this.stickyIndex ? 'position: sticky; top: 0;' : ''}">
+    <div class="campus-alert ${this.opened ? 'opened' : 'closed'}">
+      ${this.opened ? html`
+        <div class="date">${formattedDate}</div>
+        <button class="close-button" @click="${this.closeBanner}">&times; Close</button>
+        <div class="${infoButtonClass}">
+          <!-- Info button content -->
+          <svg fill="#000000" height="30px" width="30px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 27.963 27.963" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g id="c129_exclamation"> <path d="M13.983,0C6.261,0,0.001,6.259,0.001,13.979c0,7.724,6.26,13.984,13.982,13.984s13.98-6.261,13.98-13.984 C27.963,6.259,21.705,0,13.983,0z M13.983,26.531c-6.933,0-12.55-5.62-12.55-12.553c0-6.93,5.617-12.548,12.55-12.548 c6.931,0,12.549,5.618,12.549,12.548C26.531,20.911,20.913,26.531,13.983,26.531z"></path> <polygon points="15.579,17.158 16.191,4.579 11.804,4.579 12.414,17.158 "></polygon> <path d="M13.998,18.546c-1.471,0-2.5,1.029-2.5,2.526c0,1.443,0.999,2.528,2.444,2.528h0.056c1.499,0,2.469-1.085,2.469-2.528 C16.441,19.575,15.468,18.546,13.998,18.546z"></path> </g> <g id="Capa_1_207_"> </g> </g> </g></svg>
+        </div>
+        <div class="message-container">
+          <div class="slanted-card" style="display: ${this.opened ? 'flex' : 'none'}">
+            ${this.message} <!-- Display message within slanted card -->
           </div>
-        ` : html`
+        </div>
+      ` : html`
           <div class="closed-text">
             <div class="${infoButtonClass}">
               <!-- Info button content -->
@@ -221,6 +230,7 @@ export class CampusAlert extends LitElement {
           </div>
         `}
         <div class="message-container">
+          PENN STATE ALERT
           <div class="slanted-card" style="display: ${this.opened ? 'flex' : 'none'}">
             ${this.message}
           </div>
